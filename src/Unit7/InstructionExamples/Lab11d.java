cdc0ac03edd706c921b9dc0460b72357a68dbac1
INeedAUniqueUsername <alexiscomical445@gmail.com>
2017-02-15 22:44:43 -0800

    Lab11

--- src/Unit7/InstructionExamples/Lab11d.java
+++ src/Unit7/InstructionExamples/Lab11d.java
@@ -1,3 +1,6 @@
+package Unit7.InstructionExamples;
+
+
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -9,9 +12,9 @@ public class Lab11d
 {
    public static void main( String args[] )
    {
-
-
-
-
+	   for(int i = 5; i < 10; i++)
+	   {
+		   System.out.println(new TriangleFour(i, ""+(char) (i+50)));
+	   }
 	}
 }
