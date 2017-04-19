44934f6eeb685f7ee832d3f83a9f2818d321dbe6
INeedAUniqueUsername <alexiscomical445@gmail.com>
2017-04-19 11:57:38 -0700

    Moving Things

--- src/Unit17/Assignments/MovingThing.java
+++ src/Unit17/Assignments/MovingThing.java
@@ -10,49 +10,40 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public abstract class MovingThing implements Locatable {
-	private int xPos;
-	private int yPos;
-
-	public MovingThing() {
-		this(0, 0);
-	}
-
-	public MovingThing(int x, int y) {
-		setPos(x, y);
-	}
-
-	public void setPos(int x, int y) {
-		setX(x);
-		setY(y);
-	}
-
-	public void setX(int x) {
-		xPos = x;
-	}
-
-	public void setY(int y) {
-		yPos = y;
-	}
-
-	public int getX() {
-		return xPos;
-	}
-
-	public int getY() {
-		return yPos;
-	}
-
+	private int xPos, yPos;
+	public MovingThing() { this(0, 0); }
+	public MovingThing(int x, int y) { setPos(x, y); }
+	
+	public void setPos(int x, int y) { setX(x); setY(y); }
+	public void setX(int x) { xPos = x; }
+	public void setY(int y) { yPos = y; }
+	
+	public void incX(int x) { xPos += x; }
+	public void incY(int y) { yPos += y; }
+	
+	public int getX() { return xPos; }
+	public int getY() { return yPos; }
+	
 	public abstract void setSpeed(int s);
-
 	public abstract int getSpeed();
 
 	public abstract void draw(Graphics window);
 
 	public void move(String direction) {
 		// add more code to complete the move method
-		switch(direction) {
+		int speed = getSpeed();
+		switch(direction.toUpperCase()) {
 		case "LEFT":
-			setX(getX() - getSpeed());
+			incX(-speed);
+			break;
+		case "RIGHT":
+			incX(speed);
+			break;
+		case "UP":
+			incY(-speed);
+			break;
+		case "DOWN":
+			incY(speed);
 			break;
 		}
 	}
